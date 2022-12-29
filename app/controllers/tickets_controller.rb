d4d9c91363184c5456e1dc0e941958e243031e12
PrinceGiri <princegiri262@gmail.com>
2022-12-30 00:40:52 +0530

    first commit

---
+++ app/controllers/tickets_controller.rb
@@ -0,0 +1,15 @@
+class TicketsController < ApplicationController
+    def show
+        @tickets = Ticket.last
+        binding.b
+        @code = @tickets.code
+        @qrcode = RQR::QRCode.new("http://github.com/")
+        @svg = @qrcode.as_avg(
+            offset: 0,
+            color: '000',
+            shape_rendering: 'cripsEdges',
+            module_side: 6
+        )
+
+    end
+end
