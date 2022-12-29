012db03958566a8e7002b56bf5ff1e075e68a37a
PrinceGiri <princegiri262@gmail.com>
2022-12-30 01:00:49 +0530

    EC-02 fixed migration issue

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -9,6 +9,8 @@ class TicketsController < ApplicationController
             shape_rendering: 'cripsEdges',
             module_side: 6
         )
+        # This is not working properlity so i try to fixed this issue tommaro and also learn how to create QR in ruby on rails.
+        # i hope so i will fixed this issue tommaro definatly 
 
     end
 end
